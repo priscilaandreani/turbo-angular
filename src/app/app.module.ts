457563ed1f12ff0e02631ca9666643dc14d3e447
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2021-07-26 18:55:08 -0300

    🎉 start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PanelComponent } from './panel/panel.component';
+import { ProgressComponent } from './progress/progress.component';
+import { AttemptsComponent } from './attempts/attempts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PanelComponent,
+    ProgressComponent,
+    AttemptsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
